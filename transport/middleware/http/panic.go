bcacc4754e05653ac3878e41c8f77fe4edca6378
rlapenok <rlapenok@gmail.com>
2025-09-25 23:21:38 +0300

    rebuild PanicMiddleware

--- transport/middleware/http/panic.go
+++ transport/middleware/http/panic.go
@@ -1,8 +1,6 @@
 package http
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/rlapenok/toolbox/errors"
 	"go.uber.org/zap"
@@ -12,34 +10,8 @@ import (
 func PanicMiddleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		// Получаем данные из контекста
-		requestID := c.GetString("request_id")
-		method := c.Request.Method
-		path := c.Request.URL.Path
-		query := c.Request.URL.RawQuery
-		ip := c.ClientIP()
-		userAgent := c.Request.UserAgent()
-		referer := c.Request.Referer()
-		requestSize := c.Request.ContentLength
-
-		// Добавляем данные в логгер
-		midLogger := logger.With(
-			zap.String("request_id", requestID),
-			zap.String("method", method),
-			zap.String("path", path),
-			zap.String("query", query),
-			zap.String("ip", ip),
-			zap.String("user-agent", userAgent),
-			zap.String("referer", referer),
-			zap.Int64("request_size", requestSize),
-		)
-
-		startTimestamp := time.Now()
-
 		defer func() {
 			if r := recover(); r != nil {
-				responseSize := c.Writer.Size()
-				latency := time.Since(startTimestamp)
 				details := errors.NewDetails()
 				details.WithLocaleMessage("en-EN", "internal server error")
 
@@ -51,11 +23,8 @@ func PanicMiddleware(logger *zap.Logger) gin.HandlerFunc {
 				message := err.Message()
 				reason := err.Reason()
 
-				midLogger.With(
-					zap.Int("status", httpStatus),
-					zap.Duration("latency", latency),
-					zap.Int("response_size", responseSize),
-				).Error("request completed", zap.Any("panic", r))
+				logger.Error("PANIC", zap.Any("panic", r))
+
 				c.AbortWithStatusJSON(httpStatus, gin.H{
 					"code":    code,
 					"message": message,
